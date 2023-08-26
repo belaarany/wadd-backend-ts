412ae99e5e521c500bbd763cf79c509138e8dba7
Bela Arany <git@belaarany.com>
2023-08-26 18:44:44 +0200

    feat: add psql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,29 @@
 import { Module } from "@nestjs/common"
+import { ConfigModule, ConfigService } from "@nestjs/config"
 import { TypeOrmModule } from "@nestjs/typeorm"
 import { GraphQLRootModule } from "./graphql/graphql.module"
 import { HttpRootModule } from "./http/http.module"
-import { CategoriesModule } from "./services/categories/categories.module"
-import { ExpensesModule } from "./services/expenses/expenses.module"
-import { IncomesModule } from "./services/incomes/incomes.module"
-import { TransfersModule } from "./services/transfers/transfers.module"
-import { WalletsModule } from "./services/wallets/wallets.module"
 
 @Module({
 	imports: [
 		HttpRootModule,
 		GraphQLRootModule,
-		TypeOrmModule.forRoot({
-			type: "mongodb",
-			url: "mongodb://localhost",
-			port: 27017,
-			database: "wadd_expense",
-			entities: [__dirname + "/**/*.entity{.ts,.js}"],
-			useUnifiedTopology: true,
-			useNewUrlParser: true,
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: (configService: ConfigService) => {
+				return {
+					type: "postgres",
+					host: configService.get("POSTGRES_HOST", "localhost"),
+					port: configService.get("POSTGRES_PORT", 5432),
+					username: configService.get("POSTGRES_USER", "admin"),
+					password: configService.get("POSTGRES_PASSWORD", "admin"),
+					database: configService.get("POSTGRES_DB", "nestjs"),
+					entities: [__dirname + "/**/*.entity.{js,ts}"],
+					synchronize: true,
+					// logging: true,
+				}
+			},
 		}),
 	],
 	controllers: [],
